95f73734fe6e3313148e3b25a72b746430a4fe30
Gabriel Lopez <gabriel@e3io.com>
2020-04-11 20:06:14 -0400

    Applying Eval Fixes

--- QueueMe_V1.3/Web/myScripts/JavaScript.js
+++ QueueMe_V1.3/Web/myScripts/JavaScript.js
@@ -1,4 +1,12 @@
-﻿var modal = document.getElementById('notif');
+﻿var links =
+    [
+        '../Web/post.html',
+        '../Web/filter_post1.html',
+        '../Web/filter_post2.html',
+        '../Web/filter_post3.html'
+    ]
+
+var modal = document.getElementById('notif');
 
 function previewFile()
 {
@@ -23,6 +31,10 @@ window.onclick = function (event) {
     }
 }
 
+function openSite() {
+    document.getElementById('reporting').innerHTML = "<input style='width: 100px;'type='button' value='Unreport' onclick='unreport()'>";
+}
+
 function applied()
 {
     document.getElementById('application').innerHTML = "<input style='width: 100px;'type='button' value='Cancel' onclick='cancel()'>";
@@ -33,7 +45,7 @@ function cancel()
     document.getElementById('application').innerHTML = "<input style='width: 100px;' type='button' value='Apply' onclick='applied()'>";
 }
 
-function report() {
+function repor() {
     document.getElementById('reporting').innerHTML = "<input style='width: 100px;'type='button' value='Unreport' onclick='unreport()'>";
 }
 
@@ -44,4 +56,17 @@ function unreport() {
 function comment()
 {
     document.getElementById('comupload').innerHTML = "<div class='prof_title'><div class='row'><strong>Comments:</strong><div id='application' style='width: 100px; position: absolute; right: 0px;'><input style='width: 100px;' type='button' value='Submit' onclick='comment()'></div></div></div><div class='comments' style='margin-top: 20px;'><p class='prof_text'>So here is where your comment would be.</p></div><div class='prof_text'><textarea id='desc' name='desc' rows='4' cols='130' maxlength='256' style='resize: none; margin-top: 10px; margin-left: 5px; padding-top: 2.5px;'></textarea> <br /><br /></div>";
+}
+
+function rando() {
+    var myrandom = Math.round(Math.random() * 2)
+    var link1 = "http://www.codingforums.com"
+    var link2 = "http://www.cssdrive.com"
+    var link3 = "http://www.dynamicdrive.com"
+    if (myrandom == 0)
+        window.location = link1
+    else if (myrandom == 1)
+        window.location = link2
+    else if (myrandom == 2)
+        window.location = link3
 }
